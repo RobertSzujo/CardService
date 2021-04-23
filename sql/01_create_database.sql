e30efac7ec36850c073fc1f22e77564cebee2233
szujorobert99 <szujorobert99@gmail.com>
2021-04-23 12:23:10 +0200

    Changed SQL database script to use foreign keys.

--- sql/01_create_database.sql
+++ sql/01_create_database.sql
@@ -1,32 +1,32 @@
 DROP TABLE CARD;
-DROP TABLE OWNER;
 DROP TABLE CONTACT;
+DROP TABLE OWNER;
 DROP TABLE CARD_TYPE;
 DROP SEQUENCE OWNER_SEQ;
 DROP SEQUENCE CARD_TYPE_SEQ;
 DROP SEQUENCE CONTACT_SEQ;
 
-CREATE TABLE CARD
-( card_number CHAR(16) NOT NULL PRIMARY KEY,
-  valid_thru CHAR(5),
-  card_hash VARCHAR2(128),
-  card_type_id int NOT NULL,
-  owner_id int NOT NULL,
-  disabled CHAR(1) DEFAULT 'N');
-  
-CREATE TABLE OWNER
-( owner_id int NOT NULL PRIMARY KEY,
+CREATE TABLE OWNER (
+  owner_id int NOT NULL PRIMARY KEY,
   owner VARCHAR2(128) );
-
-CREATE TABLE CONTACT
-( contact_id int NOT NULL PRIMARY KEY,
-  owner_id int NOT NULL,
+  
+CREATE TABLE CARD_TYPE (
+  card_type_id int NOT NULL PRIMARY KEY,
+  card_type VARCHAR2(128) );
+  
+CREATE TABLE CONTACT (
+  contact_id int NOT NULL PRIMARY KEY,
+  owner_id int REFERENCES OWNER (owner_id),
   contact_type VARCHAR(5) NOT NULL,
   contact VARCHAR2(128) );
-  
-CREATE TABLE CARD_TYPE
-( card_type_id int NOT NULL PRIMARY KEY,
-  card_type VARCHAR2(128) );
+
+CREATE TABLE CARD (
+  card_number CHAR(16) NOT NULL PRIMARY KEY,
+  valid_thru CHAR(5),
+  card_hash VARCHAR2(128),
+  card_type_id int REFERENCES CARD_TYPE (card_type_id),
+  owner_id int REFERENCES OWNER (owner_id),
+  disabled CHAR(1) DEFAULT 'N' );
   
 CREATE SEQUENCE OWNER_SEQ
   MINVALUE 1
