90d185a1e5b10d097793c9dde301567df876b00a
szujorobert99 <szujorobert99@gmail.com>
2021-04-30 10:31:00 +0200

    Removed logger import from RestCardVerificationService as it is not used.

--- application/cardservice/src/main/java/hu/robi/cardservice/service/RestCardVerificationService.java
+++ application/cardservice/src/main/java/hu/robi/cardservice/service/RestCardVerificationService.java
@@ -8,8 +8,6 @@ import hu.robi.cardservice.entity.RestCard;
 import hu.robi.cardservice.entity.RestContact;
 import org.apache.commons.validator.routines.CreditCardValidator;
 import org.apache.commons.validator.routines.EmailValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -21,8 +19,6 @@ public class RestCardVerificationService {
 
     private final CardRepository cardRepository;
     private final CardTypeRepository cardTypeRepository;
-    //define fields
-    Logger logger = LoggerFactory.getLogger(RestCardVerificationService.class);
 
     //define constructor
 
