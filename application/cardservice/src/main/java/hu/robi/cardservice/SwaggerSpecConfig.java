52cd64c0dcb5b18a9430cc55ed36db4e29f83341
szujorobert99 <szujorobert99@gmail.com>
2021-05-05 12:11:52 +0200

    Integrated swagger docs into the application. Updated setup and instruction manual.

---
+++ application/cardservice/src/main/java/hu/robi/cardservice/SwaggerSpecConfig.java
@@ -0,0 +1,33 @@
+package hu.robi.cardservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerSpecConfig {
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider(InMemorySwaggerResourcesProvider defaultResourcesProvider) {
+        return () -> {
+            SwaggerResource wsResource = new SwaggerResource();
+            wsResource.setName("api");
+            wsResource.setSwaggerVersion("2.0");
+            wsResource.setLocation("/swagger.yaml");
+
+            List<SwaggerResource> resources = new ArrayList<>();
+            resources.add(wsResource);
+            return resources;
+        };
+    }
+
+}
